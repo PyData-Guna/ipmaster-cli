4f617419e9a27f4f8037042a9741b0cce703b4a3
Param Siddharth <30315706+paramsiddharth@users.noreply.github.com>
2021-10-28 10:33:16 +0530

    Add entrypoint (#8)
    Signed-off-by: Param Siddharth <contact@paramsid.com>

--- src/ipmaster/entry.py
+++ src/ipmaster/entry.py
@@ -1,10 +1,7 @@
-import socket
-from urllib.parse import urlparse
-try:
-    WebpageLink = input()
-    url = 'http://www.example.com/site/section1/VAR1/VAR2' 
-    WebpageLink = urlparse(url).netloc
-    IP_addres = socket.gethostbyname(WebpageLink)
-    print("IP Address is:" + IP_addres)
-except:
-    print("error occured")
+from sys import argv
+
+def main(args: list[str]):
+	...
+
+if __name__ == '__main__':
+	main(argv[1:])
